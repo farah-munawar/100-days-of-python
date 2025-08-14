bb63fb177ad3574e50723fa8bbf00b06871b494d
Farah Munawar <farahmunawar04@gmail.com>
2025-08-14 19:45:34 +0500

    Implement working turtle race game

---
+++ Turtle-Race-Game/main.py
@@ -0,0 +1,37 @@
+import random
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+
+is_race_on = False
+user_bet = screen.textinput('Make your bet', 'Which turtle will win the race? Enter a color: ')
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+y_positions = [125, 75, 25, -25, -75, -125]
+all_turtles = []
+
+for i in range(6):
+    t = Turtle(shape='turtle')
+    t.color(colors[i])
+    t.penup()
+    t.goto(x=-230, y=y_positions[i])
+    all_turtles.append(t)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for racer in all_turtles:
+        rand_distance = random.randint(0, 10)
+        racer.forward(rand_distance)
+
+        if racer.xcor() > 230:
+            is_race_on = False
+            winning_color = racer.pencolor()
+            if user_bet and winning_color.lower() == user_bet.lower():
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost. The {winning_color} turtle won.")
+
+screen.exitonclick()
