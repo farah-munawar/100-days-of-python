2bf45bf151ee27f9c78d3b8f32a367a182468f0f
Farah Munawar <farahmunawar04@gmail.com>
2025-08-15 19:28:00 +0500

    Completed Snake game: added movement controls, game loop, and direction handling

--- Snake-Game/main.py
+++ Snake-Game/main.py
@@ -1,5 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,27 +10,37 @@ screen.bgcolor("black")
 screen.title('My Snake Game')
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-turtles = []
 
-for position in starting_positions:
-    t = Turtle('square')
-    t.color('white')
-    t.penup()
-    t.goto(position)
-    turtles.append(t)
+snake=Snake()
+food=Food()
+scoreboard=Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    for i in range(len(turtles) - 1, 0, -1):
-        x = turtles[i - 1].xcor()
-        y = turtles[i - 1].ycor()
-        turtles[i].goto(x, y)
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-    turtles[0].forward(20)
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_is_on=False
+        scoreboard.gameover()
 
-
+    for t in snake.turtles:
+        if t==snake.head:
+            pass
+        elif snake.head.distance(t)<10:
+            game_is_on=False
+            scoreboard.gameover()
 screen.exitonclick()
---
+++ Snake-Game/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
+
+class Snake:
+    def __init__(self):
+        self.turtles = []
+        self.create_snake()
+        self.head = self.turtles[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_turtle(position)
+
+    def add_turtle(self,position):
+            t = Turtle('square')
+            t.color('white')
+            t.penup()
+            t.goto(position)
+            self.turtles.append(t)
+    def extend(self):
+        self.add_turtle(self.turtles[-1].position())
+
+
+    def move(self):
+        for i in range(len(self.turtles) - 1, 0, -1):
+            x = self.turtles[i - 1].xcor()
+            y = self.turtles[i - 1].ycor()
+            self.turtles[i].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
